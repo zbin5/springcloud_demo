9a2997e13e2bc6809e9fe5b7fa1c711b8b447c01
zbin <1784642708@qq.com>
2021-09-09 15:54:05 +0800

    ..

--- zb-springcloud/provider1/src/main/java/com/zb/springcloud/provider1/filter/SecurityConfig.java
+++ zb-springcloud/provider1/src/main/java/com/zb/springcloud/provider1/filter/SecurityConfig.java
@@ -1,47 +1,10 @@
-//package com.zb.springcloud.provider1.filter;
-//
-//import com.zb.springcloud.register.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//@Configuration
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Autowired
-//    private MyAuthenticationProvider authenticationProvider;
-//
-//    /**
-//     * 注入用户信息服务
-//     * @return 用户信息服务对象
-//     */
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        return (UserDetailsService) new UserService();
-//    }
-//
-//    @Override
-//    @Bean
-//    public AuthenticationManager authenticationManagerBean() throws Exception {
-//        return super.authenticationManagerBean();
-//    }
-//
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers(HttpMethod.OPTIONS,"/oauth/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .csrf()
-//                .disable();
-//    }
-//}
+package com.zb.springcloud.provider1.filter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
